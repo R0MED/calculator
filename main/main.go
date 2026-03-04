36e1481d1a65cff416e86e24375d72cf372ddedd
NABABA <romden2006@gmail.com>
2026-03-04 08:13:26 -0800

    redisign

--- main/main.go
+++ main/main.go
@@ -5,17 +5,26 @@ import (
 )
 
 func main() {
-	var num1 int
-	var num2 int
+	var num1, num2 float64     
 	var action string
 
 	fmt.Print("Введите первое число: ")
-	fmt.Scan(&num1)
+	_, err := fmt.Scan(&num1)
+	if err != nil {
+		fmt.Println("Ошибка ввода первого числа")
+		return
+	}
 
 	fmt.Print("Введите второе число: ")
-	fmt.Scan(&num2)
+	_, err = fmt.Scan(&num2)
+	if err != nil {
+		fmt.Println("Ошибка ввода второго числа")
+		return
+	}
 
-	fmt.Print("Введите действие (плюс +, минус -, делить /, умножить *, остато от деления %, степень ^): ")
-	fmt.Scan(&action)
-
-}
+	fmt.Print("Введите действие (+ - * / % ^): ")
+	_, err = fmt.Scan(&action)
+	if err != nil {
+		fmt.Println("Ошибка ввода действия")
+		return
+	}
