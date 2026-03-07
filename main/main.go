c6220cab786811adf5d9e0c3e1ea34946b0fbed4
NABABA <romden2006@gmail.com>
2026-03-07 02:07:31 -0800

    final

--- main/main.go
+++ main/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var num1, num2 float64     
+	var num1, num2 float64
 	var action string
 
 	fmt.Print("Введите первое число: ")
@@ -28,3 +29,39 @@ func main() {
 		fmt.Println("Ошибка ввода действия")
 		return
 	}
+
+	var result float64
+	var valid bool = true
+
+	switch action {
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	case "*":
+		result = num1 * num2
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Ошибка: деление на 0")
+			valid = false
+		} else {
+			result = num1 / num2
+		}
+	case "%":
+		if num2 == 0 {
+			fmt.Println("Ошибка: деление на 0 (остаток)")
+			valid = false
+		} else {
+			result = float64(int64(num1) % int64(num2))
+		}
+	case "^":
+		result = math.Pow(num1, num2)
+	default:
+		fmt.Printf("Ошибка: неизвестная операция '%s'\n", action)
+		valid = false
+	}
+
+	if valid {
+		fmt.Printf("%.4f %s %.4f = %.4f\n", num1, action, num2, result)
+	}
+}
